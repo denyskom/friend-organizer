e93207f25f8ecc7c43ae9473c3404241b88a2934
Denys Kompaniiets <denyskom@interlink.office>
2018-09-17 14:33:36 +0300

    Add project files.

---
+++ FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FriendOrganizer.Model;
+
+namespace FriendOrganizer.UI.Data
+{
+    class FriendDataService
+    {
+        public IEnumerable<Friend> GetAll()
+        {
+            yield return new Friend { FirstName = "Thomas", LastName = "Huber" };
+            yield return new Friend { FirstName = "Thomas", LastName = "Huber" };
+            yield return new Friend { FirstName = "Thomas", LastName = "Huber" };
+            yield return new Friend { FirstName = "Thomas", LastName = "Huber" };
+            yield return new Friend { FirstName = "Thomas", LastName = "Huber" };
+
+        }
+    }
+}
